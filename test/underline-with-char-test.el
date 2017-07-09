8b89f293f4dc08cc7e944cb4ffc8578b1678af91
Marco Wahl <marcowahlsoft@gmail.com>
2017-07-09 12:09:44 +0200

    *:fresh generation

--- test/underline-with-char-test.el
+++ test/underline-with-char-test.el
@@ -6,21 +6,19 @@
 ;; :END:
 
 
-;; [[id:17c5897e-3413-4576-aa83-3869e0cb1053][Concrete tests:1]]
+;; [[file:~/p/elisp/mw/underline-with-char/underline-with-char.org::*Concrete%20tests][Concrete tests:1]]
 (require 'underline-with-char)
 
-(ert-deftest f2149b5d7e74f04715435e3767bb5b28eb973ab6 ()
-
+(ert-deftest 88bded2c526dcf44116420e7a33eb7ab58b905ee ()
   (should
-   (equal
-    "lala
-----"
+   (string=
+    "a
+-"
     (with-temp-buffer
-      (let ((underline-with-char-fill-char ?-))
-       (insert "lala
+      (insert "a
 ")
-       (call-interactively #'underline-with-char)
-       (buffer-substring (point-min) (point-max)))))))
+        (underline-with-char ?-)
+        (buffer-substring (point-min) (point-max))))))
 
 (ert-deftest 1cf806ca781aba4a68e248c4a3c5a0bd3017ea75 ()
   (should
@@ -28,22 +26,9 @@
     "lala
 la--"
     (with-temp-buffer
-      (let ((underline-with-char-fill-char ?-))
-        (insert "lala
+      (insert "lala
 la")
-        (call-interactively #'underline-with-char)
-        (buffer-substring (point-min) (point-max)))))))
-
-(ert-deftest 88bded2c526dcf44116420e7a33eb7ab58b905ee ()
-  (should
-   (string=
-    "a
-a"
-    (with-temp-buffer
-      (let ((underline-with-char-fill-char ?a))
-        (insert "a
-")
-        (call-interactively #'underline-with-char))
+        (underline-with-char ?-)
         (buffer-substring (point-min) (point-max))))))
 
 (ert-deftest eb74280b23db3a9ac18032d641aa280a6cb2c4c1 ()
@@ -54,7 +39,7 @@ a"
     (with-temp-buffer
       (insert "lala
 ")
-      (underline-with-char-choose nil ? )
+      (underline-with-char ? )
       (buffer-substring (point-min) (point-max))))))
 
 (ert-deftest 69756cd95b0b4ad224f728d57799253664e1c79c ()
@@ -62,13 +47,13 @@ a"
    (equal
     "lala
 ++++
-++++"
+++--"
     (with-temp-buffer
       (insert "lala
 ")
-      (underline-with-char-choose '(4) ?+)
-      (insert "\n")
-      (underline-with-char)
+      (underline-with-char ?+)
+      (insert "\n++")
+      (underline-with-char ?-)
       (buffer-substring (point-min) (point-max))))))
 ;; Concrete tests:1 ends here
 
--- underline-with-char.el
+++ underline-with-char.el
@@ -8,7 +8,7 @@
 ;; :END:
 
 
-;; [[id:17c5897e-3413-4576-aa83-3869e0cb1053][Program:1]]
+;; [[file:~/p/elisp/mw/underline-with-char/underline-with-char.org::*Program][Program:1]]
 ;; THIS FILE HAS BEEN GENERATED.
 
 
@@ -18,103 +18,32 @@
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: convenience
 
-;; There are two functions
-;;
-;; 1. `underline-with-char' uses the internal setting for the fill-char.
-;; 2. `underline-with-char-choose' allows to choose the fill-char.
+;; This program provides just command `underline-with-char'.
 
-;; E.g. with `underline-with-char-fill-char' set to '-' and point
-;; symbolized as <!> and starting with
+;; Example:
+;;
+;; Let <!> be the location of the Emacs cursor aka point.
 ;;
 ;; ;; Worthy to be underlined
 ;; ;; <!>
 ;;
 ;; then
 ;;
-;; M-x underline-with-char
+;; M-x underline-with-char -
 ;;
 ;; yields
 ;;
 ;; ;; Worthy to be underlined
 ;; ;; -----------------------
 
-;; You can also set a character for the next underline using function
-;; `underline-with-char-choose'.
-
-;; Example
-;; _______
-
-;; ;; Worthy to be underlined
-;; ;; <!>
-;;
-;; then
-;;
-;; M-x underline-with-char-choose _
-;;
-;; yields
-;;
-;; ;; Worthy to be underlined
-;; ;; _______________________
-
-;; You can also set the underline character for subsequent calls to `underline-with-char'.
-;; Example
-;; _______
-
-;; Worthy to be underlined two times
-;; <!>
-;;
-;; then
-;;
-;; C-u M-x underline-with-char-choose X
-;; RET
-;; M-x underline-with-char
-
-;; yields
-;;
-;; Worthy to be underlined two times
-;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 
 ;;; Code:
 
 
-(defcustom underline-with-char-fill-char ?-
-  "The character for the underline."
-  :group 'underline-with-char
-  :type 'character)
-
-
 ;;;###autoload
-(defun underline-with-char ()
-  "Underline the line above with a certain character.
-
-The character is defined by `underline-with-char-fill-char'.
-
-Fill what's remaining if not at the first position.
-
-E.g. with `underline-with-char-fill-char' set to '-' and point
-symbolized as <!> and starting with
-
-;; Commentary:
-;; <!>
-
-get
-
-;; Commentary:
-;; -----------"
-  (interactive)
-  (underline-with-char-choose nil underline-with-char-fill-char))
-
-;;;###autoload
-(defun underline-with-char-choose (arg char)
-  "Underline the line above with a certain character.
-
-Fill what's remaining if not at the first position.
-
-With prefix ARG use the CHAR for subsequent calls to
-`underline-with-char'"
-  (interactive  "P\ncchar: ")
+(defun underline-with-char (char)
+  "Underline the line above with a certain character."
+  (interactive  "cchar: ")
   (insert
    (make-string
     (save-excursion
@@ -126,9 +55,7 @@ With prefix ARG use the CHAR for subsequent calls to
           (forward-char col)))
       (let ((old-point (point)))
         (- (progn (end-of-line) (point)) old-point)))
-    char))
-    (if (equal '(4) arg)
-      (setq underline-with-char-fill-char char)))
+    char)))
 
 
 (provide 'underline-with-char)
